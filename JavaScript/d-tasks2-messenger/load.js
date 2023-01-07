3674bc7ee292992f32dab4b49a7a925ab91fc828
Vladyslav Karpenko <klarpen@gmail.com>
2023-01-07 11:50:16 +0200

    Switch back to custom loader & cleanup it

--- JavaScript/d-tasks2-messenger/load.js
+++ JavaScript/d-tasks2-messenger/load.js
@@ -3,13 +3,11 @@
 const fs = require('node:fs').promises;
 const vm = require('node:vm');
 
-module.exports = async (filePath, sandbox, { RUN_OPTIONS, console }) => {
+module.exports = (options) => async (filePath, sandbox) => {
   const src = await fs.readFile(filePath, 'utf8');
   const code = `'use strict';\n${src}`;
   const script = new vm.Script(code, { filename: filePath, lineOffset: -1 });
-  console.log({ script });
   const context = vm.createContext(Object.freeze({ ...sandbox }));
-  const exported = script.runInContext(context, RUN_OPTIONS);
-  console.log({ exported });
+  const exported = script.runInContext(context, options);
   return exported;
 };
--- JavaScript/d-tasks2-messenger/main.js
+++ JavaScript/d-tasks2-messenger/main.js
@@ -3,6 +3,7 @@
 const fsp = require('node:fs').promises;
 const path = require('node:path');
 const config = require('./config.js');
+const load = require('./load.js')(config.SANDBOX_RUN_OPTIONS);
 const server = require(`./transport/${config.transport}.js`);
 const staticServer = require('./static.js');
 const db = require('./db.js');
@@ -27,13 +28,10 @@ const routing = {};
 (async () => {
   const files = await fsp.readdir(apiPath);
   for (const fileName of files) {
-    if (!fileName.endsWith('.mjs')) continue;
+    if (!fileName.endsWith('.js')) continue;
     const filePath = path.join(apiPath, fileName);
-    const serviceName = path.basename(fileName, '.mjs');
-    const {
-      default: serviceDIContainer
-    } = await import(filePath);
-    routing[serviceName] = await serviceDIContainer(Object.freeze({ ...sandbox}));
+    const serviceName = path.basename(fileName, '.js');
+    routing[serviceName] = await load(filePath, Object.freeze({ ...sandbox}));
     logger.log('Service { name: %s, methods: [%s] }', serviceName, Object.keys(routing[serviceName]).join(', '));
   }
 
