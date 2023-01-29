f850e7a4651ac48e99f72b8bd46593e7dd2546c8
Vladyslav Karpenko <klarpen@gmail.com>
2023-01-29 11:02:26 +0200

    Cleanup previous tasks folders in branch that's not related to Metaschema

--- JavaScript/b-task1.6-switch-to-native-loader/api/city.mjs
+++
@@ -1,7 +0,0 @@
-const diContainer = async ({ console, db, common}) => (
-  // Service code starts from here
-  db('city')
-  // Service code ends there
-);
-
-export default diContainer;
--- JavaScript/b-task1.6-switch-to-native-loader/consoleProvider.js
+++
@@ -1,3 +0,0 @@
-const logger = require('./config.js').logger;
-if (logger === 'native') module.exports = console;
-else module.exports = require(`./${logger}.js`);
